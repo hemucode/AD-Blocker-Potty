153a852814a77b4cc95b25538157c89b1750943b
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-01-15 15:58:00 +0530

    ok

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# AD Blocker Potty
-
-# What the AD Blocker Potty does
+## What the AD Blocker Potty does 💩
 
 🚫 Give yourself faster browsing (as resources are blocked from loading)
 
 🚫 Blocks all ads, including text ads (including Facebook adblock), rich media ads, pop-ups, banners, and video ads (including YouTube adblock).
+
 🚫 Thanks to the absence of ads and pop-up windows, speeds up page loading and conserves bandwidth.
 
 🚫 Prevents malware and phishing attacks; Blocks many spyware, adware, and dialer installers. 
@@ -12,18 +11,29 @@
 🚫 Protects your privacy by preventing common third-party tracking systems.
 
 
-# How to remove AD Blocker Potty social media widgets?
+✅ How to remove AD Blocker Potty social media widgets?
 
 Tired of seeing "Like" buttons and other similar widgets on all the websites you visit frequently? Simply turn on AD Blocker Potty's "Social media filter" to ignore them.
  
-# How can Blocker Potty protect you from online threats?
+✅ How can Blocker Potty protect you from online threats?
 
 At the moment we’ve got more than 2,500,000 harmful websites on record. AD Blocker Potty can block domains known to spread malware, protecting your computer against viruses, Trojan horses, worms, spyware, and adware. AD Blocker Potty really lowers the risk of virus infections and prohibits access to harmful websites to prevent potential attacks.
 
 
-# Why do I need to grant permissions to AD Blocker Potty?
+✅ Why do I need to grant permissions to AD Blocker Potty?
 
 "Access your data for all websites" and "Access browser tabs": These two permissions are required for AD Blocker Potty to perform any type of post-processing on the content of the pages. Simple ad server blocking by naive ad blocking would leave broken elements and first-party ads on the pages. AD Blocker Potty applies specific aesthetic standards to make pages appear neat and orderly. "Access browser activity during navigation" is a permission required to monitor navigational events and to implement rules as needed.
 
+✅ Release notes: 
+https://www.downloadhub.cloud/2022/12/ad-blocker.html
+
+
+✅ Free and Open Source:
+https://github.com/hemucode/AD-Blocker-Potty
+
+
+✅ Found a bug? Have a problem and need help? Visit our forum: https://www.downloadhub.cloud/
+
 
 ✅Please Note: When installing AD Blocker Potty, you will receive a warning that AD Blocker Potty will receive access to your browsing data. This is a standard message - we NEVER collect any user information.
+
---
+++ opera/lib/service.js
@@ -0,0 +1,127 @@
+chrome.runtime.onStartup.addListener(async () => {
+  chrome.action.setBadgeText({text: ''});
+  var a = new Promise(function(resolve, reject){
+        chrome.storage.sync.get({"enabled": true}, function(options){
+            resolve(options.enabled);
+      })
+  });
+  const enabled = await a;
+  if (enabled) {
+    await enable();
+  } else {
+    await disable();
+  }
+});
+
+
+
+chrome.runtime.onInstalled.addListener(async (details) => {
+  switch (details.reason) {
+    case chrome.runtime.OnInstalledReason.INSTALL:
+      return chrome.storage.sync.set({
+        installDate: Date.now(),
+        installVersion: chrome.runtime.getManifest().version,
+      });
+    case chrome.runtime.OnInstalledReason.UPDATE:
+      return chrome.storage.sync.set({
+        updateDate: Date.now(),
+      });
+  }
+});
+
+chrome.runtime.onMessage.addListener(async (request, sender) => {
+  switch (request.action) {
+    case "INSERT_FILTER": {
+      chrome.action.setBadgeText({text: ''});
+      return  chrome.declarativeNetRequest.updateEnabledRulesets({
+          enableRulesetIds:["ruleset_1","ruleset_13","ruleset_14","ruleset_16","ruleset_2","ruleset_224","ruleset_3","ruleset_7","ruleset_8","ruleset_9"]
+        },
+        () => {}
+      );
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(m, s, c){
+  if (m.blockedCount) { 
+    n = m.blockedCount; 
+    chrome.action.setBadgeBackgroundColor({ color: 'white' });
+    chrome.action.setBadgeText({text: m.blockedCount.toString(), tabId: s.tab.id});
+  }
+})
+
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+  if (changeInfo.status == 'complete') {
+  chrome.action.setBadgeText({text: ''});
+  let n = 0;  
+    chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(function (o) {
+      if (n>9999) {n = 0}
+      console.log('rule matched:', o);
+      chrome.action.setBadgeBackgroundColor({ color: 'white' });
+      chrome.action.setBadgeText({text: n.toString()});
+      ++n
+    })
+  }
+})
+
+
+
+chrome.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "sync") return;
+
+  if (changes.enabled) {
+    if (changes.enabled.newValue) {
+      await enable();
+    } else {
+      await disable();
+    }
+  }
+});
+async function enable() {
+  await chrome.action.setIcon({
+    path: {
+      32: "data/icons/icon-32.png",
+      38: "data/icons/icon-38.png",
+      128: "data/icons/icon-128.png",
+    },
+  });
+
+  chrome.declarativeNetRequest.updateEnabledRulesets({
+          enableRulesetIds:["ruleset_1","ruleset_13","ruleset_14","ruleset_16","ruleset_2","ruleset_224","ruleset_3","ruleset_7","ruleset_8","ruleset_9"]
+        },
+        () => {}
+  );
+  await reloadAffectedTab();
+}
+
+async function disable() {
+  await chrome.action.setIcon({
+    path: {
+      32: "data/icons/icon-disabled-32.png",
+      38: "data/icons/icon-disabled-38.png",
+      128: "data/icons/icon-disabled-128.png",
+    },
+  });
+  n = 0;
+  chrome.action.setBadgeText({text: ''});
+  chrome.declarativeNetRequest.updateEnabledRulesets({
+          disableRulesetIds:["ruleset_1","ruleset_13","ruleset_14","ruleset_16","ruleset_2","ruleset_224","ruleset_3","ruleset_7","ruleset_8","ruleset_9"]
+        },
+      () => {}
+  );
+  await reloadAffectedTab();
+}
+
+async function reloadAffectedTab() {
+  const [currentTab] = await chrome.tabs.query({
+    active: true,
+    url: "*://*/*",
+  });
+  const isTabAffected = Boolean(currentTab?.url);
+  if (isTabAffected) {
+    return chrome.tabs.reload();
+  }
+}
+
